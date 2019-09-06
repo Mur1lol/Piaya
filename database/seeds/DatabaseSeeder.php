0bf97ee793425178491a4464f66530a305c35f69
Murilo <bc.murilo.mbc@gmail.com>
2019-09-05 21:28:14 -0300

    Adicionando seed ao projeto

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+        DB::insert('INSERT INTO denuncias (problema, tipo, lixeira, acontecimento, local) VALUES (?,?,?,?,?)', 
+        	array ('Descarte incorreto de lixo ou residuos','Papel','Nao reciclavel','Aluno jogou papel na lixeira errada','Sala 01'));
+
+    	DB::insert('INSERT INTO denuncias (problema, tipo, lixeira, acontecimento, local) VALUES (?,?,?,?,?)', 
+        	array ('Uso inadequado da luz','Luz acesa em ambiente vazio','--',
+        		'Aula acabou mais cedo e ninguem apagou as luzes','Sala 02'));
+
+        DB::insert('INSERT INTO denuncias (problema, tipo, lixeira, acontecimento, local) VALUES (?,?,?,?,?)', 
+        	array ('Problemas relacionados a agua','Torneira aberta','--','O banheiro ficou todo inundado','Banheiro'));
+        
+        DB::insert('INSERT INTO denuncias (problema, tipo, lixeira, acontecimento, local) VALUES (?,?,?,?,?)', 
+        	array ('Descarte incorreto de lixo ou residuos','Perfurocortante','Reciclavel',
+        		'Objeto afiado em lixeira de reciclaveis','Sala 10'));
+
+        DB::insert('INSERT INTO denuncias (problema, tipo, lixeira, acontecimento, local) VALUES (?,?,?,?,?)', 
+        	array ('Uso inadequado da luz','Ar condicionado ligado em sala vazia','--','Tava ligado','Sala 07'));
+        
+        DB::insert('INSERT INTO denuncias (problema, tipo, lixeira, acontecimento, local) VALUES (?,?,?,?,?)', 
+        	array ('Problemas relacionados a agua', 'Vazamento', '--', 'Torneira estragada', 'Banheiro'));
+        
+        DB::insert('INSERT INTO denuncias (problema, tipo, lixeira, acontecimento, local) VALUES (?,?,?,?,?)', 
+        	array ('Descarte incorreto de lixo ou residuos', 'Quimico', 'Reciclavel',
+        		'Recipiente inapropriado ', 'Laboratorio de Quimica'));
     }
 }
